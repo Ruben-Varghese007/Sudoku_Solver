4d0de13227d4091e013f4433e2f5dc1588d66b66
Ruben George Varghese <116579510+Ruben-Varghese007@users.noreply.github.com>
2024-09-13 00:40:30 +0530

    Update sudoku_deploy.py

--- sudoku_deploy.py
+++ sudoku_deploy.py
@@ -4,7 +4,7 @@ import time
 
 # Set up the Streamlit page
 st.title("Sudoku Solver (Backtracking)")
-st.subheader("This app generates a random Sudoku puzzle and solves it using a Backtracking Algorithm.")
+st.subheader("This app generates a random Sudoku puzzle and solves it using Backtracking Algorithm.")
 st.write("Name:  [ Ruben George Varghese ]")
 st.write("GitHub: [ https://github.com/Ruben-Varghese007/Sudoku_Solver ]")
 st.divider()
